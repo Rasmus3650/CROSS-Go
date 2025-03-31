83f63b4d24db5a317a843ad10c1e42a7b67a56e6
Rasmus3650 <rasmus3650@gmail.com>
2025-03-31 13:00:15 +0200

    Added all test cases for merkle trees, need to fix the error, which is probably append()

--- debug_CROSS_submission/Additional_Implementations/Benchmarking/main.c
+++ debug_CROSS_submission/Additional_Implementations/Benchmarking/main.c
@@ -13,7 +13,7 @@ void print_array(uint8_t *arr, size_t size) {
     printf("\n");
 }
 
-void merkleTestSpeed(){
+/*void merkleTestSpeed(){
     const char *seeds[] = {
         "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
         "HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM",
@@ -25,13 +25,13 @@ void merkleTestSpeed(){
     for (int i = 0; i < T; i++) {
         randombytes(leaves[i],HASH_DIGEST_LENGTH);
     }
-    //printf("Leaves: \n");
-   /* for (int i = 0; i < T; i++) {
+    printf("Leaves: \n");
+   for (int i = 0; i < T; i++) {
         print_array(leaves[i], HASH_DIGEST_LENGTH);
-    }*/
+    }
     tree_root(root, leaves);
-    //printf("Root: \n");
-    //print_array(root,HASH_DIGEST_LENGTH);
+    printf("Root: \n");
+    print_array(root,HASH_DIGEST_LENGTH);
     printf("Chall_2: \n");
     
 
@@ -40,21 +40,54 @@ void merkleTestSpeed(){
     const char *input = seeds[j];
     expand_digest_to_fixed_weight(chall_2,input);
     uint8_t proof[W*HASH_DIGEST_LENGTH];
-    //print_array(chall_2,T);
+    print_array(chall_2,T);
     tree_proof(proof,leaves,chall_2);
     printf("Proof: \n");
     for (int i = 0; i < W; i++) {
         print_array(proof + i*HASH_DIGEST_LENGTH, HASH_DIGEST_LENGTH);
     }
     }
-}
+}*/
 
 void merkleTest(){
+    const char *seeds[] = {
+        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        "HMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM",
+        "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+    };
+    for (int j = 0; j < 3; j++) {
+        uint8_t root[HASH_DIGEST_LENGTH];
+        uint8_t leaves[T][HASH_DIGEST_LENGTH];
+        for (int i = 0; i < T; i++) {
+            randombytes(leaves[i],HASH_DIGEST_LENGTH);
+        }
+        printf("Leaves: \n");
+        for (int i = 0; i < T; i++) {
+            print_array(leaves[i], HASH_DIGEST_LENGTH);
+        }
+        uint8_t tree[NUM_NODES_MERKLE_TREE * HASH_DIGEST_LENGTH] = {0};
+        tree_root(root, tree, leaves);
+        printf("Root: \n");
+        print_array(root,HASH_DIGEST_LENGTH);
+        printf("Chall_2: \n");
     
+
+        //tree_proof
+        uint8_t chall_2[T]={0};
+        const char *input = seeds[j];
+        expand_digest_to_fixed_weight(chall_2,input);
+        uint8_t proof[HASH_DIGEST_LENGTH*TREE_NODES_TO_STORE] = {0};
+        print_array(chall_2,T);
+        tree_proof(proof, tree, chall_2);
+        printf("Proof: \n");
+        for (int i = 0; i < TREE_NODES_TO_STORE; i++) {
+            print_array(proof + i*HASH_DIGEST_LENGTH, HASH_DIGEST_LENGTH);
+        }
+    }
 }
 
 
 int main() {
-    merkleTestSpeed();
+    merkleTest();
     return 0;
 }
